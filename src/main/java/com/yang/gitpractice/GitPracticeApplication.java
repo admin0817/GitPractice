b1cdf8395d07911c4490bc744db05243b52a890b
admin0817 <41173245+admin0817@users.noreply.github.com>
2019-06-03 15:58:21 +0800

    Update GitPracticeApplication.java

--- src/main/java/com/yang/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/yang/gitpractice/GitPracticeApplication.java
@@ -3,6 +3,7 @@ package com.yang.gitpractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// spring boot启动主类
 @SpringBootApplication
 public class GitPracticeApplication {
 
