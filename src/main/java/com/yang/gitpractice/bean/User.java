b3d77016ae6e42e663d5e948e6d66711a542039d
yjiao <yjiao@g2link.cn>
2019-06-03 15:01:51 +0800

    修改

--- src/main/java/com/yang/gitpractice/bean/User.java
+++ src/main/java/com/yang/gitpractice/bean/User.java
@@ -1,8 +1,16 @@
 package com.yang.gitpractice.bean;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @author jiaoYang
  * @create 2019-06-03 14:49
  */
+@Data
+@ToString
 public class User {
+
+    private String name;
+
 }
