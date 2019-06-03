3b25fb729008e4d21e0a0f9c4bc4eed3610cf5a6
yjiao <yjiao@g2link.cn>
2019-06-03 15:59:26 +0800

    删除注释

--- src/main/java/com/yang/gitpractice/GitPracticeApplication.java
+++ src/main/java/com/yang/gitpractice/GitPracticeApplication.java
@@ -3,7 +3,6 @@ package com.yang.gitpractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// spring boot启动主类
 @SpringBootApplication
 public class GitPracticeApplication {
 
